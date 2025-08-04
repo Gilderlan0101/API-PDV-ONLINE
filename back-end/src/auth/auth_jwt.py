86c2add47b593492458b0d1e42288768ad69fba5
Gilderlan0101 <lansilva007gg@gmail.com>
2025-08-04 12:21:48 -0300

    Este arquivo cria um token de acesso. com ele podemos cria outras validaçoes em breve.

---
+++ back-end/src/auth/auth_jwt.py
@@ -0,0 +1,58 @@
+from passlib.context import CryptContext
+import os
+from datetime import datetime, timedelta
+from typing import Optional, Union, Any
+from jose import jwt
+from zoneinfo import ZoneInfo
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Chamando as variaves de ambiete que vmos usar neste arquivo
+ALGORITHM = os.getenv('ALGORITHM', 'HS256')
+JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'secret')
+JWT_REFRESH_SECRET_KEY = os.getenv('JWT_REFRESH_SECRET_KEY', 'refresh_secret')
+ACCESS_TOKEN_EXPIRE_MINUTES = 60  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7   # 7 days
+
+
+password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def get_hashed_password(password: str) -> str:
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_pass: str) -> bool:
+    return password_context.verify(password, hashed_pass)
+
+
+def create_access_token(
+    subject: Union[str, Any], expires_delta: Optional[int] = None
+) -> str:
+    expire = (
+        datetime.now(ZoneInfo('America/Sao_Paulo'))
+        + timedelta(minutes=expires_delta)
+        if expires_delta
+        else datetime.now(ZoneInfo('America/Sao_Paulo'))
+        + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    to_encode = {'exp': expire, 'sub': str(subject)}
+    return jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+
+
+def create_refresh_token(
+    subject: Union[str, Any], expires_delta: Optional[int] = None
+) -> str:
+    expire = (
+        datetime.now(ZoneInfo('America/Sao_Paulo'))
+        + timedelta(minutes=expires_delta)
+        if expires_delta
+        else datetime.now(ZoneInfo('America/Sao_Paulo'))
+        + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+    )
+
+    to_encode = {'exp': expire, 'sub': str(subject)}
+    return jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
