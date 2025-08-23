7a39e55dc6ff9714a37d8e1497211785375c5242
Gilderlan <lansilva007gg@gmail.com>
2025-08-23 20:52:01 -0300

    Criamos essa rota para atualizar dados de produtos que ja estão no carrinho. Caso o usuario envie apens quantidade ou outro dado. apenas esse dado será atualizado no carrinho e mantemos os outros.

---
+++ src/routes/car/adjustments.py
@@ -0,0 +1,22 @@
+# routes/cart_update.py
+from fastapi import APIRouter, Depends
+from src.auth.deps import get_current_user
+from src.model.user import Usuario
+from src.controllers.car.cart_control import CartManagerDB
+from src.schemas.carrinho import EditCartItem
+
+router = APIRouter()
+cart = CartManagerDB()
+
+@router.post("/atualizar")
+async def atualizar_item(
+    item: EditCartItem,
+    current_user: Usuario = Depends(get_current_user),
+):
+    return await cart.update_produto(
+        product_id=item.product_id,
+        user_id=current_user.id, # type: ignore
+        quantity=item.quantity,
+        discount=item.discount,
+        addition=item.addition,
+    )
