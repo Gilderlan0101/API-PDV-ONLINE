d10ecf0dde091cabd75809b69f82f58c770ebb60
Gilderlan0101 <lansilva007gg@gmail.com>
2025-09-15 10:51:48 -0300

    Testando rota add produtos ao carinho

--- src/routes/car/create.py
+++ src/routes/car/create.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Query
 from src.auth.deps import get_current_user
 from src.model.user import Usuario
+from src.model.employee import Employees
+
 from src.controllers.car.cart_control import CartManagerDB
 
 router = APIRouter(tags=["Carrinho"])
@@ -14,6 +16,11 @@ async def adicionar_produto(
     current_user: Usuario = Depends(get_current_user),
 ):
     """
-    Adiciona um produto ao carrinho do usuário.
+    Adiciona um produto ao carrinho do usuário. ou fucuinario
     """
-    return await cart.add_produto(product_id, quantity, current_user.id)
+
+    fucionario = await Employees.filter(id=current_user.id).first()
+    admin = fucionario.usuario_id
+
+    if admin != None:
+        return await cart.add_produto(product_id, quantity, fucionario.usuario_id)
